13735ce60376ed3d52e6ae65cd4875d591a63761
TinHtunOo <tinhtunhe@gmail.com>
2024-12-02 12:50:51 +0700

    first upload

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
-function App() {
+export default function App() {
+  const [from, setFrom] = useState("USD");
+  const [to, setTo] = useState("EUR");
+  const [amount, setAmount] = useState(0);
+  const [rate, setRate] = useState(0);
+  const [error, setError] = useState("");
+
+  useEffect(
+    function () {
+      async function getRate() {
+        try {
+          const res = await fetch(
+            `https://api.frankfurter.dev/v1/latest?base=${from}&symbols=${to}`
+          );
+          if (!res.ok) throw new Error("Bad Currency Pair.");
+          const data = await res.json();
+          setError("");
+          setRate(data.rates[to]);
+        } catch (error) {
+          console.error(error.message);
+          setError(error.message);
+        }
+      }
+      getRate();
+    },
+    [from, to]
+  );
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h2>Currency Converter</h2>
+      <div className="box">
+        <span>From</span>
+        <input
+          type="text"
+          value={amount}
+          onChange={(e) => setAmount(Number(e.target.value))}
+        />
+        <select value={from} onChange={(e) => setFrom(e.target.value)}>
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+          <option value="CAD">CAD</option>
+          <option value="INR">INR</option>
+        </select>
+        <span>to</span>
+        <select value={to} onChange={(e) => setTo(e.target.value)}>
+          <option value="USD">USD</option>
+          <option value="EUR">EUR</option>
+          <option value="CAD">CAD</option>
+          <option value="INR">INR</option>
+        </select>
+      </div>
+      <h3>{!error ? `${(amount * rate).toFixed(2)} ${to}` : `${error}`}</h3>
     </div>
   );
 }
-
-export default App;
